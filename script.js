92f8b50c4d112173a2d1f8488459b5aaffbe380f
Ashish Mishra <ashish.mishra12012007@gmail.com>
2024-09-25 19:42:49 +0530

    website prototype

---
+++ script.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Desktop } from "./Desktop"; // Ensure the path is correct
+
+function App() {
+  return (
+    <div>
+      <Desktop />
+    </div>
+  );
+}
+
+export default App;
